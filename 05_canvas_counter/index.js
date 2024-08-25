25a9c12bb8f1a87f9a8717998948b4fa4e840188
seo-K <dkasid@naver.com>
2024-08-25 14:51:22 +0900

    feat: 이펙트 투명도 0일때 제거하기

--- 05_canvas_counter/index.js
+++ 05_canvas_counter/index.js
@@ -41,13 +41,23 @@ function render() {
     if (delta < interval) return;
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    particles.forEach((particle, index) => {
-      particle.update();
-      particle.draw(ctx);
+    // 이 경우 particles 의 index 가 삭제되면서 밀리면서, 사이드이펙트가 생김
+    // particles.forEach((particle, index) => {
+    //   particle.update();
+    //   particle.draw(ctx);
+    //   console.log(particles);
 
-      // 파티클의 opacity가 0 이하면 삭제되도록 하기. (아니면 opacity가 0인채로 계속 남아있게되기때문)
-      if (particle < 0) particles.splice(index, 1);
-    });
+    //   // 파티클의 opacity가 0 이하면 삭제되도록 하기. (아니면 opacity가 0인채로 계속 남아있게되기때문)
+    //   // if (particle < 0) particles.splice(index, 1);
+    // });
+
+    // 해당하는 index 만 삭제하면 이슈가 사라짐
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].draw(ctx);
+
+      if (particles[i] < 0) particles.splice(i, 1);
+    }
 
     then = now - (delta % interval);
   };
