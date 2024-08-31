50843b6c9a6456e1b6a6aab33f86c2998630cf44
Seo-K <77239319+seo-K@users.noreply.github.com>
2024-08-31 05:02:59 +0000

    feat: 콘페티 오브젝트 구현하기

--- 06_confetti/index.js
+++ 06_confetti/index.js
@@ -21,15 +21,43 @@ const render = () => {
   let now, delta;
   let then = Date.now();
 
+  const x = innerWidth;
+  let y = innerHeight;
+  // const x = innerWidth / 2;
+  // let y = innerHeight / 2;
+  let widthAlpah = 0;
+  const width = 50;
+  const height = 50;
+  let deg = -0.1;
+
   const frame = () => {
     // 재귀적으로 스스로 계속 실행시킴. 144hz 1초에 144번 60hz 1초에 60번 실행된다. 두 모니터에 동일하게 보여짐.
     requestAnimationFrame(frame);
     delta = now - then;
     if (delta < interval) return;
+    // ctx.clearRect(0, 0, canvasWidth , canvasHeight );
+    ctx.clearRect(0, 0, canvasWidth * dpr, canvasHeight * dpr);
+
+    widthAlpah += 0.1;
+    deg += 0.1;
+    y += 1;
+
+    ctx.translate(x + width, y + height);
+    ctx.rotate(deg);
+    ctx.translate(-x - width, -y - height);
 
     // 생성
     ctx.fillStyle = "blue";
-    ctx.fillRect(200, 200, 50, 50);
+    //  Math.cos(n) 결과값은 1 ~ -1 사이를 왔다갔다한다.
+    // ctx.fillRect(x, y, width, height);
+    ctx.fillRect(
+      x,
+      y,
+      width * Math.cos(widthAlpah),
+      height * Math.sin(widthAlpah)
+    );
+    ctx.resetTransform();
+
     then = now - (delta % interval);
   };
 
